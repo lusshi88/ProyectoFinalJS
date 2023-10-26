c6c7fc0abfe2dac0be5bf65de01f95b95af4838b
Luis Bravo <bravoluis439@ymail.com>
2023-10-26 08:43:48 -0300

    no funca

--- colajs.js
+++ colajs.js
@@ -4,8 +4,6 @@ let buscador = document.getElementById("buscador");
 let modalCarrito = document.getElementById("modalCarrito");
 let precioTotal = document.getElementById("precioTotal");
 
-
-
 // Funciones exclusivas para los gustos de gaseosas del DOM!
 class GaseosasdeCola {
   constructor(id, nombre, precio, litros, imagen) {
@@ -26,26 +24,24 @@ const estanteria = [];
 // estanteria.push(cola1, cola2, cola3, cola4);
 
 // carrito ----------------------------------------------------------------
-const productoscarrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
-console.log(productoscarrito);
+// const productoscarrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
+// console.log(productoscarrito);
 
-const cargarGaseosasJson  = async()=> {
-  const resp = await fetch(`./cola.json`);
+const mostrarGustosCola = async (array) => {
+  const resp = await fetch(`../cola.json`);
   const datagaseosa = await resp.json();
-} 
 
-  let estanteria2 = datagaseosa
+  let estanteria2 = datagaseosa;
+
   // Aca imprimo (con un for of) lo que se ve en gaseosascola.html con el DOM
-  function mostrarGustosCola(array) {
-    paginaCola.innerHTML = ""
-    
-    
-    //for of: para recorrer un array posición a posición
-    for (let gaseosa of estanteria2) {
-      
-      let mostrargaseosascolaDiv = document.createElement("div");
-      mostrargaseosascolaDiv.className = "col-12 col-md-6 col-lg-4 mb-5 ";
-      mostrargaseosascolaDiv.innerHTML = ` <div id="${gaseosa.id}" class="cardd " style="width: 18rem;">
+
+  paginaCola.innerHTML = "";
+
+  //for of: para recorrer un array posición a posición
+  for (let gaseosa of estanteria2) {
+    let mostrargaseosascolaDiv = document.createElement("div");
+    mostrargaseosascolaDiv.className = "col-12 col-md-6 col-lg-4 mb-5 ";
+    mostrargaseosascolaDiv.innerHTML = ` <div id="${gaseosa.id}" class="cardd " style="width: 18rem;">
         <img class="card-img-top img-fluid" style="height: 250px;"src="../imagenes/${gaseosa.imagen}" alt="Gaseosas de cola">
         <div class="card-body">
             <h4 class="card-title"></h4>
@@ -55,69 +51,59 @@ const cargarGaseosasJson  = async()=> {
             <button type="button"  class="btn btn-primary" id="btncompra${gaseosa.id}" >COMPRAR</button>
          </div>
     </div> `;
-  
-     paginaCola.append(mostrargaseosascolaDiv);
-  
-  // todo para el carrito ----------------------------------------------------------------
-  let btncompras = document.getElementById(`btncompra${GaseosasdeCola.id}`);
-  
-  
-  btncompras.addEventListener("click", () => {
-  agregarAlCarrito(GaseosasdeCola)
-    
-  })
+
+    paginaCola.append(mostrargaseosascolaDiv);
+
+    // todo para el carrito ----------------------------------------------------------------
+    // let btncompras = document.getElementById(`btncompra${GaseosasdeCola.id}`);
+
+    // btncompras.addEventListener("click", () => {
+    // agregarAlCarrito(GaseosasdeCola)
+
+    // })
   }
-  }
+};
+mostrarGustosCola();
 
-
-
-cargarGaseosasJson()
 // función para calcular el total de los productos
-function calcularTotal (array) {
-const totalReduce = array.reduce(
-(acumulador, GaseosasdeCola) =>
-{return acumulador + GaseosasdeCola.precio},
-0  
-)
-precioTotal.innerHTML = `El total de su compra es $${totalReduce}`
+function calcularTotal(array) {
+  const totalReduce = array.reduce((acumulador, GaseosasdeCola) => {
+    return acumulador + GaseosasdeCola.precio;
+  }, 0);
+  precioTotal.innerHTML = `El total de su compra es $${totalReduce}`;
 }
-
-
 
 // funcion cuando ponen mas de 1 gaseosa iguales
 
 function agregarAlCarrito(elementocola) {
-  let gaseosaduplicada = productoscarrito.find((cola)=> cola.id == elementocola.id)
-  
-  gaseosaduplicada == undefined ?
+  let gaseosaduplicada = productoscarrito.find(
+    (cola) => cola.id == elementocola.id
+  );
 
-  (// aca estoy pusheando al carrito
-  productoscarrito.push(elementocola),
-  // pusheo al storage
-  localStorage.setItem("carrito",JSON.stringify(productoscarrito)),
-  Toastify({
-    text: `Se agrego ${elementocola.nombre} al carrito `,
-    duration: 2500,
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    style: {
-      background: "linear-gradient(to right,red,blue,red )",
-      color: "white",
-    },
-    onClick: function () {}, // Callback after click
-  }).showToast()):
-   noticarrito2(productoscarrito)
+  gaseosaduplicada == undefined
+    ? // aca estoy pusheando al carrito
+      (productoscarrito.push(elementocola),
+      // pusheo al storage
+      localStorage.setItem("carrito", JSON.stringify(productoscarrito)),
+      Toastify({
+        text: `Se agrego ${elementocola.nombre} al carrito `,
+        duration: 2500,
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        style: {
+          background: "linear-gradient(to right,red,blue,red )",
+          color: "white",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast())
+    : noticarrito2(productoscarrito);
 
- 
- 
-
-
-function cargarProductosCarrito (array){
-  modalCarrito.innerHTML = "";
-array.forEach((elementoCarrito) => {
-  modalCarrito.innerHTML += `
+  function cargarProductosCarrito(array) {
+    modalCarrito.innerHTML = "";
+    array.forEach((elementoCarrito) => {
+      modalCarrito.innerHTML += `
 <div class="card border-primary mb-3" id =${elementoCarrito.id} style="max-width: 540px;">
      <img class="card-img-top" height="250"   src="../imagenes/${elementoCarrito.imagen}" alt="">
      <div class="card-body">
@@ -127,59 +113,58 @@ array.forEach((elementoCarrito) => {
              <button class= "btn btn-danger" id="btneliminar${elementoCarrito.id}"><i class="fas fa-trash-alt"></i></button>
      </div>    
 </div>
-`
+`;
+    });
+    array.forEach((elementoCarrito) => {
+      document
+        .getElementById(`btneliminar${elementoCarrito.id}`)
+        .addEventListener("click", () => {
+          // lo borro del dom
+          let borrarcard = document.getElementById(`${elementoCarrito.id}`);
+          borrarcard.remove();
+          // lo borro del array
+          let posicionArray = array.indexOf(elementoCarrito);
+          array.splice(posicionArray, 1);
+          // lo borro del storage
+          localStorage.setItem("carrito", JSON.stringify(array));
+          calcularTotal(array);
+        });
+    });
 
-}
-)
-array.forEach((elementoCarrito) => {
-  document.getElementById(`btneliminar${elementoCarrito.id}`).addEventListener("click", () => {
-  // lo borro del dom
-  let borrarcard = document.getElementById(`${elementoCarrito.id}`)
-  borrarcard.remove()
-// lo borro del array
-let posicionArray = array.indexOf(elementoCarrito)
-array.splice(posicionArray,1)
-// lo borro del storage
-localStorage.setItem("carrito",JSON.stringify(array))
-calcularTotal(array)
-})
+    calcularTotal(array);
+  }
 
-}
-)
+  botonCarrito.addEventListener("click", () => {
+    cargarProductosCarrito(productoscarrito);
+  });
 
-calcularTotal(array)
+  function noticarrito2(elemento) {
+    Toastify({
+      text: `Ya existe este pruducto en el carrito `,
+      duration: 2500,
+      newWindow: true,
+      close: true,
+      gravity: "bottom", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      style: {
+        background: "linear-gradient(to right,red,blue,red )",
+        color: "white",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+  }
 
-}
+  // mostrarGustosCola(estanteria2);
 
-botonCarrito.addEventListener("click",()=>{cargarProductosCarrito(productoscarrito)})
+  // funcion para buscar por input las gaseosas
 
-function noticarrito2 (elemento) {
-  Toastify({
-    text: `Ya existe este pruducto en el carrito `,
-    duration: 2500,
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    style: {
-      background: "linear-gradient(to right,red,blue,red )",
-      color: "white",
-    },
-    onClick: function () {}, // Callback after click
-  }).showToast();
-}
-
-
-   
-// mostrarGustosCola(estanteria2);
-
-// funcion para buscar por input las gaseosas
-
-function buscainfo(buscado, array) {
-  let coincidencias = array.filter(gaseosa => gaseosa.nombre.toLowerCase().includes(buscado.toLowerCase()))
-  mostrarGustosCola(coincidencias);
-}
-buscador.addEventListener("input", () => {
-  buscainfo(buscador.value, estanteria);
-});
+  function buscainfo(buscado, array) {
+    let coincidencias = array.filter((gaseosa) =>
+      gaseosa.nombre.toLowerCase().includes(buscado.toLowerCase())
+    );
+    mostrarGustosCola(coincidencias);
+  }
+  buscador.addEventListener("input", () => {
+    buscainfo(buscador.value, estanteria);
+  });
 }
